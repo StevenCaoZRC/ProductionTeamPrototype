f3967141f8fca84388ada549b88fa09770f30a59
SugarZRC <31192307+SugarZzzzzz@users.noreply.github.com>
2019-02-21 12:26:07 +1300

    Rotates smoothly now but need to make it rotate first then move

--- Prototype 1/Assets/Scripts/PlayerMovement.cs
+++ Prototype 1/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
 {
     public Camera m_camera; //grabs the main camera
     public NavMeshAgent agent;
-    public float speed = 0.5f;
-
+    public float speed = 2.0f;
+    Vector3 targetDir;
+    RaycastHit hit;
+    Vector3 rayHitPoint;
     // Start is called before the first frame update
     void Start()
     {
-       
+        rayHitPoint = transform.forward;
     }
 
     // Update is called once per frame
@@ -21,25 +23,36 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = m_camera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+           
             
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider != null)
                 {
-                    Vector3 targetDir = hit.collider.transform.position - transform.position;
+                    targetDir = hit.collider.transform.position - transform.position;
                     if(Mathf.Abs(targetDir.x) >= 0.9f || Mathf.Abs(targetDir.z) >= 0.9f) //If player is moving more than 1 square
                     {
-                        //
-                        Debug.Log("target: x: " + targetDir.x + " y: " + targetDir.y + " z: " + targetDir.z);
-                        Vector3 dir = Vector3.Slerp(transform.forward, hit.collider.transform.position, speed * Time.deltaTime);
-                        transform.rotation = Quaternion.LookRotation(dir);
-                        agent.SetDestination(hit.collider.gameObject.transform.position);
+                        rayHitPoint = hit.point;
+
 
                     }
+
+            
+
                 }
+               
+                    
             }
                 
          }
-    }
+        Debug.Log("target: x: " + targetDir.x + " y: " + targetDir.y + " z: " + targetDir.z);
+
+        Quaternion _targetDir = Quaternion.LookRotation(rayHitPoint - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, _targetDir, speed * Time.deltaTime);
+       
+           
+
+       agent.SetDestination(hit.collider.gameObject.transform.position);
+    }       
+
 }
