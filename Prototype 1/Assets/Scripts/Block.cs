30dd70e0b4a03c249ce59abfa6826689d742d952
Vivian <vivianngo12@hotmail.com>
2019-02-22 11:52:51 +1300

    Block scripting -water and fire BUT needs to be coded to work with movement and fire control needs to work with fireblock script

---
+++ Prototype 1/Assets/Scripts/Block.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockType { Ground, Grass, Water, Ice, Vine, Fire };
+
+public class Block : MonoBehaviour
+{
+    public BlockType m_blockType = BlockType.Ground;
+    public bool m_isWalkable = true;
+
+    public BlockType GetBlockType() { return m_blockType; }
+    public void SetBlockType(BlockType _type) { m_blockType = _type; }
+
+    public bool GetWalkable() { return m_isWalkable; }
+    public void SetWalkable(bool _walkable) { m_isWalkable = _walkable; }
+
+    public virtual void Reset()
+    {
+        m_isWalkable = false;
+        m_blockType = BlockType.Ground;
+    }
+
+}
---
+++ Prototype 1/Assets/Scripts/FireBlock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBlock : Block
+{
+    bool fireBurning = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Reset()
+    {
+        fireBurning = true;
+        m_isWalkable = false;
+        m_blockType = BlockType.Fire;
+    }
+
+    public void PutOutFire()
+    {
+        if(fireBurning)
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            fireBurning = false;
+            m_isWalkable = true;
+            m_blockType = BlockType.Ground;
+        }
+        
+    }
+}
