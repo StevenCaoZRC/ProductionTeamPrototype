6c37baa519e83524d13cc208004680a2a9e2e22e
Vivian <vivianngo12@hotmail.com>
2019-02-28 11:11:38 +1300

    main menu new //hasnt been changed

---
+++ Prototype 1/Assets/Scripts/MenuScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScreen : MonoBehaviour
+{
+    public GameObject MainMenuUI;
+    public GameObject HelpMenuUI;
+    public GameObject CreditsMenuUI;
+
+    public void Start()
+    {
+        GameManager.SetInMenu(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void LoadMainMenu()
+    {
+        MainMenuUI.SetActive(true);
+        HelpMenuUI.SetActive(false);
+        CreditsMenuUI.SetActive(false);
+    }
+    public void LoadHelpMenu()
+    {
+        HelpMenuUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void LoadCreditsMenu()
+    {
+        CreditsMenuUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
