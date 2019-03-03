cb602ff6bb4f295d0736baa14ab43cf59182a40d
SugarZzzzzz <31192307+SugarZzzzzz@users.noreply.github.com>
2019-03-03 23:19:46 +1300

    Imported sound, Audiomanager established

---
+++ Prototype 1/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    //Singleton
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);//prevents 
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.pitch = s.pitch;
+            s.source.volume = s.volume;
+        }
+    }
+    void Start()
+    {
+        
+    
+    }
+    public void Play(string name)
+    {
+       Sound s = Array.Find(sounds, sound => sound.name == name);
+       s.source.Play();
+    }
+}
